36ef16be1d152f1b0731799c7ba41027a9e7a876
Marouane Fagri <mfagri@e2r4p9.1337.ma>
2022-06-04 19:07:52 +0100

    i make my global variable

--- global_variable.c
+++ global_variable.c
@@ -6,7 +6,7 @@
 /*   By: mfagri <mfagri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 18:57:26 by mfagri            #+#    #+#             */
-/*   Updated: 2022/06/04 19:03:03 by mfagri           ###   ########.fr       */
+/*   Updated: 2022/06/04 19:07:22 by mfagri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,3 @@ int	get_glo(int i)
 		return (j);
 	return (j);
 }
-
-"
-this function can be used instead of global variable.
- 
-if i==0
-the function return 0
-if i ==1
-the function return 1
-else
-the function return the value stored in the static variable.
-
-Static variables have a property of preserving their value even after they are out of their scope! Hence, static variables preserve their previous value in their previous scope and are not initialized again in the new scope. 
-Syntax: 
-
-static data_type var_name = var_value;
-"
